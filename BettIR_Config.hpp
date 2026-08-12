3034a1e4580fb3b8dae38af79845db0f5bb32582
prazv <64920464+prazv@users.noreply.github.com>
2026-08-12 05:36:36 +0300

    Add files via upload

--- BettIR_Config.hpp
+++ BettIR_Config.hpp
@@ -3,30 +3,60 @@ class BettIR_Config
 	class CompatibleNightvisionGoggles
 	{
 		class __base_NVG;
-		class rhsusf_ANPVS_14_wp: __base_NVG
+		class MIG_PVS31_LOW: __base_NVG
 		{
 		};
-		class rhsusf_ANPVS_15_wp: __base_NVG
+		class MIG_PVS31_LOW_Up: __base_NVG
 		{
 		};
-		class rhsusf_anvis_nvg_bc_caps: __base_NVG
+		class MIG_PVS31_STD: __base_NVG
 		{
 		};
-		class rhsusf_ANVIS: __base_NVG
+		class MIG_PVS31_DayOps_STD: __base_NVG
 		{
 		};
-		class rhsusf_ANVIS_wp: __base_NVG
+		class MIG_PVS14_DayOps_B: __base_NVG
 		{
 		};
-		class rhsusf_anvis_nvg_bc_caps_wp: __base_NVG
+		class MIG_PVS14_B: __base_NVG
 		{
 		};
-		class UK3CB_ANPVS7: __base_NVG
+		class MIG_PVS14_DayOps_L: __base_NVG
 		{
 		};
-		class UK3CB_PVS5A: __base_NVG
+		class MIG_PVS14_L: __base_NVG
 		{
 		};
+		class MIG_PVS14_DayOps_R: __base_NVG
+		{
+		};
+		class MIG_PVS14_R: __base_NVG
+		{
+		};
+		class MIG_FPANO_BLK: __base_NVG
+		{
+		};
+		class MIG_FPANO_DayOps_BLK: __base_NVG
+		{
+		};
+		class MIG_FPANO_FDE: __base_NVG
+		{
+		};
+		class MIG_FPANO_DayOps_FDE: __base_NVG
+		{
+		};
+		class MIG_GPNVG18_BLK: __base_NVG
+		{
+		};
+		class MIG_GPNVG18_DayOps_BLK: __base_NVG
+		{
+		};
+		class MIG_GPNVG18_FDE: __base_NVG
+		{
+		};
+		class MIG_GPNVG18_DayOps_FDE: __base_NVG
+		{
+		};	
 		class JAS_GPNVG18_blk: __base_NVG
 		{
 		};
@@ -36,13 +66,91 @@ class BettIR_Config
 		class JAS_GPNVG18_blk_TI: __base_NVG
 		{
 		};
+		class JAS_GPNVG18_blk_TI_WP: __base_NVG
+		{
+		};
 		class JAS_GPNVG18_Tan_TI: __base_NVG
+		{
+		};
+		class JAS_GPNVG18_Tan_TI_WP: __base_NVG
 		{
 		};
 		class JAS_GPNVG18_Tan_WP: __base_NVG
 		{
 		};
 		class JAS_GPNVG18_blk_WP: __base_NVG
+		{
+		};
+		class UK3CB_ANPVS7: __base_NVG
+		{
+		};
+		class UK3CB_ANPVS7_wp: __base_NVG
+		{
+		};
+		class UK3CB_PVS5A: __base_NVG
+		{
+		};
+		class UK3CB_PVS5A_wp: __base_NVG
+		{
+		};
+		class cbwgo_nv33_bat_cov: __base_NVG
+		{
+		};
+		class cbwgo_nv33_bat_nocov: __base_NVG
+		{
+		};
+		class cbwgo_nv33_nobat_cov: __base_NVG
+		{
+		};
+		class cbwgo_nv33_nobat_nocov: __base_NVG
+		{
+		};
+		class cbwgo_bonie_cov: __base_NVG
+		{
+		};
+		class cbwgo_bonie_nocov: __base_NVG
+		{
+		};
+		class cbwgo_nv33_bat_cov_wp: __base_NVG
+		{
+		};
+		class cbwgo_nv33_bat_nocov_wp: __base_NVG
+		{
+		};
+		class cbwgo_nv33_nobat_cov_wp: __base_NVG
+		{
+		};
+		class cbwgo_nv33_nobat_nocov_wp: __base_NVG
+		{
+		};
+		class cbwgo_bonie_cov_wp: __base_NVG
+		{
+		};
+		class cbwgo_bonie_nocov_wp: __base_NVG
+		{
+		};
+		class rhsusf_ANPVS_14_wp: __base_NVG
+		{
+		};
+		class rhsusf_ANPVS_15_wp: __base_NVG
+		{
+		};
+		class rhsusf_anvis_nvg_bc_caps: __base_NVG
+		{
+		};
+		class rhsusf_anvis_nvg_bc_caps_wp: __base_NVG
+		{
+		};
+		class rhsusf_ANPVS_14: __base_NVG
+		{
+		};
+		class rhsusf_ANPVS_15: __base_NVG
+		{
+		};
+		class rhsusf_1PN138: __base_NVG
+		{
+		};
+		class rhsusf_1PN138_WP: __base_NVG
 		{
 		};
 	};
--- config.cpp
+++ config.cpp
@@ -1,4 +1,4 @@
+#include "cfgMods.hpp"
 #include "CfgPatches.hpp"
 #include "CfgWeapons.hpp"
 #include "BettIR_Config.hpp"
-#include "cfgMods.hpp"
